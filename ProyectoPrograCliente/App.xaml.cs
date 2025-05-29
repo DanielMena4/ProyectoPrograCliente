c96bd23a3fc27642fd91816a6fac95dbf016192a
Geovany <Geovany@GEOVANY-TOSHIBA>
2025-05-29 16:26:09 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoPrograCliente/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoPrograCliente
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ProyectoPrograCliente/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoPrograCliente
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
